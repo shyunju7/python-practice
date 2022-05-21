ddabe9e6af721121c47a0b8e06718bb5d752a3aa
shyunju7 <shyunju7@naver.com>
2022-05-22 00:38:15 +0900

    Add baekjoon2929.py

--- pythonProject/baekjoon/baekjoon2929.py
+++ pythonProject/baekjoon/baekjoon2929.py
@@ -1,16 +1,4 @@
-# baekjoon 10491 Quite a problem
-# 브론즈 2
-# 문자열 파싱
-import sys
-input = sys.stdin.readline
-while True:
-    line = input().rstrip()
-    if line == "EOI":
-        break
-
-    if line.upper().find("NEMO") != -1:
-            print("Found")
-    else:
-        print("Missing")
-
+# baekjoon 2929 머신 코드
+# 브론즈 1
+# 구현 문자열 파싱 정규 표현식
 
