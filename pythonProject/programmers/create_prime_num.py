47bab5a775c79d6fcd9c324582910125b50a2d43
shyunju7 <shyunju7@naver.com>
2022-05-25 18:06:10 +0900

    Solution programmers/create_prime_num.py

--- pythonProject/programmers/create_prime_num.py
+++ pythonProject/programmers/create_prime_num.py
@@ -1,6 +1,18 @@
-def solution(numbers):
-    answer = 45 - sum(numbers)
+def is_prime(n):
+    for j in range(2, n):
+        if n % j == 0:
+            return False
+    return True
+
+
+def solution(nums):
+    answer = 0
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                if is_prime(nums[i] + nums[j] + nums[k]):
+                    answer += 1
     return answer
 
 
-print(solution([1, 2, 3, 4, 6, 7, 8, 0]))
+print(solution([1, 2, 7, 6, 4]))
