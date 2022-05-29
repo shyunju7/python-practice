2f2a8644e3f4a2906b88845c9a1ce366a985245a
shyunju7 <shyunju7@naver.com>
2022-05-29 22:04:24 +0900

    Solution baekjoon10820.py

--- pythonProject/baekjoon/baekjoon10820.py
+++ pythonProject/baekjoon/baekjoon10820.py
@@ -2,6 +2,17 @@
 # 브론즈 2
 # 구현 문자열
 # 소문자 대문자 숫자 공백
+import re
 
+while True:
+    try:
+        line = input()
+        lower_case_count = len(re.findall(r"[a-z]", line))
+        upper_case_count = len(re.findall(r"[A-Z]", line))
+        num_count = len(re.findall(r"\d", line))
+        space_count = len(re.findall(" ", line))
 
+        print(lower_case_count, upper_case_count, num_count, space_count)
 
+    except EOFError:
+        break
